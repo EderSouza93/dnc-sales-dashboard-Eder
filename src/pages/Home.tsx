eb12557bd655a161ff151f7c4fce8069b52b013c
Ederaldo Souza <84224107+EderSouza93@users.noreply.github.com>
2025-01-25 13:01:59 -0300

    :sparkles: feat: Creating Avatar List components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,33 @@
-import { CardComponent, Header } from '@/components'
+import { AvatarList, CardComponent, Header } from '@/components'
 import { Container } from '@mui/material'
+import { currencyConverter } from '@/utils'
 
 function Home() {
+  const mockListData = [
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 1',
+      subtitle: currencyConverter(1234.54),
+    },
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 2',
+      subtitle: currencyConverter(2000.54),
+    },
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 3',
+      subtitle: currencyConverter(3000.54),
+    },
+  ]
   return (
     <>
       <Header />
       <Container maxWidth="lg">
         <CardComponent>CARD</CardComponent>
+        <CardComponent>
+          <AvatarList listData={mockListData} />
+        </CardComponent>
       </Container>
     </>
   )
